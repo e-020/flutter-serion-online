59fca0112df7ab1724acabf08955d0cb79978e20
Eslam Sameh <85193717+e-020@users.noreply.github.com>
2025-04-28 00:39:23 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,195 +1,26 @@
+// استيراد مكتبة فلاتر الخاصة بالتصميم
 import 'package:flutter/material.dart';
+import 'package:first/navbar.dart';
 
+
+// الدالة الأساسية التي يبدأ منها التطبيق
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp()); // شغل تطبيقنا الرئيسي MyApp
 }
 
+// تعريف كلاس التطبيق الرئيسي
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // منشئ الكلاس مع مفتاح (key) للتعرف على الواجهة
 
   @override
   Widget build(BuildContext context) {
+    // هنا نبني واجهة التطبيق
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Store",
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          actions: [
-            Icon(
-              Icons.notifications,
-              color: Colors.white,
-              size: 30.0,
-            ),
-            SizedBox(width: 10.5),
-          ],
-          backgroundColor: Colors.black12,
-          elevation: 0.0,
-          iconTheme: IconThemeData(color: Colors.black, size: 30),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            scrollDirection: Axis.vertical,
-            children: [
-              ListTile(
-                leading: Icon(
-                  Icons.info,
-                  color: Colors.blue,
-                  size: 20.0,
-                ),
-                title: Text(
-                  'Information',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20.0,
-                  ),
-                ),
-                trailing: Text(
-                  'I',
-                  style: TextStyle(color: Colors.blue),
-                ),
-                onTap: () {},
-              )
-            ],
-          ),
-        ),
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            ListTile(
-              leading: Icon(
-                Icons.movie,
-                color: Colors.blue,
-                size: 30.0,
-              ),
-              title: Text(
-                'فيلم كوري جديد',
-                style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-             subtitle: Text(
-               'اختر من بطاقات الافلام',
-               style: TextStyle(
-                 fontSize: 14.0,
-                 color: Colors.red,
-                 fontWeight: FontWeight.bold
-               ),
-
-             ),
-              trailing: Icon(
-                Icons.play_arrow,
-                color: Colors.green,
-              ),
-            ),
-
-            Container(
-              height: 200.0,
-              width: 200.0,
-              margin: EdgeInsets.only(top: 5.0, bottom: 5.0),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-
-                children: [
-
-                  SizedBox(width: 15.0),
-                  MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
-                      onTap: () {
-                        print("Study Group clicked!");
-                      },
-                      child: Container(
-                        width: 170.0,
-                        height: 150.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          image: DecorationImage(
-                            image: NetworkImage('https://m.media-amazon.com/images/M/MV5BZDJmMDU0MTEtNmZkOS00ZGViLWExYTctYWY5YWQ0NTZlZTdhXkEyXkFqcGc%40._V1_FMjpg_UX1000_.jpg'), // استبدل هذا الرابط برابط صورة البوستر
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Text(
-                            'Study Group',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.bold,
-
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-
-
-                  SizedBox(width: 15.0),
-                  MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
-                      onTap: () {
-                        print(" ًWeak hero  clicked!");
-                      },
-                      child: Container(
-                        width: 170.0,
-                        height: 150.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          image: DecorationImage(
-                            image: NetworkImage('https://m.media-amazon.com/images/M/MV5BZDJmMDU0MTEtNmZkOS00ZGViLWExYTctYWY5YWQ0NTZlZTdhXkEyXkFqcGc%40._V1_FMjpg_UX1000_.jpg'), // استبدل هذا الرابط برابط صورة البوستر
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Weak Hero Class 1',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.bold,
-
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-
-
-
-
-
-
-
-                ],
-
-
-
-
-
-              ),
-            ),
-
-
-          ],
-        ),
-      ),
+      title: 'مشروعي', // عنوان التطبيق (يظهر في بعض الأنظمة)
+      debugShowCheckedModeBanner: false, // يخفي شعار "Debug" من أعلى الشاشة
+      home: const Navbar(), // الصفحة الأولى التي تظهر وهي HomePage
     );
   }
 }
+
+
